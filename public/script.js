9a3084bcd98a28faeaa1b92bdd8d254607b593e2
ml-delaurier <ml.delaurier@gmail.com>
2025-06-28 10:46:59 -0600

    UI improvments and text mods

--- public/script.js
+++ public/script.js
@@ -1,1 +1,7 @@
-// This project is a pure CSS-based dynamic interactive book that simulates page-flipping animations using cutting-edge features available in modern Chrome (134–135+). It seamlessly combines several powerful CSS technologies to create a fully scrollable, sprite-driven animated experience resembling a book with turning pages.
+
+gsap.registerPlugin(ScrambleTextPlugin)
+
+gsap.to(".container > h1", {
+  duration: 1,
+  scrambleText: "The Seven Truths"
+});
